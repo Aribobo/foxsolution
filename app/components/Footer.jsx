06e599dd205816c2a2dfd6767e01f8077480d1b1
Arinze Stanley <60940697+Aribobo@users.noreply.github.com>
2026-08-27 10:02:22 +0100

    Update contact numbers in Footer component
    Number changed

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -29,10 +29,10 @@ const Footer = () => {
                 <a href="mailto:support@foxcontinental.com">
                   <AiOutlineMail className=" flex text-3xl cursor-pointer hover:text-[#fd7062] " />
                 </a>
-                <a href=" https://wa.me/+17123517895">
+                <a href=" https://wa.me/+447535212848">
                   <FaWhatsapp className=" flex text-3xl cursor-pointer hover:text-green-700" />
                 </a>
-                <a href="tel:+17123517895">
+                <a href="tel:+447535212848">
                   <FiPhoneCall className=" flex text-3xl cursor-pointer hover:text-[#fd7062]" />
                 </a>
               </div>
@@ -89,7 +89,7 @@ const Footer = () => {
                 {t("contact")}
               </p>
               <li className="  text-start  text-xl  font-semibold py-2 flex ">
-               +1 7123517895
+              +447535212848
               </li>
               <li className=" text-start  text-xl  font-semibold py-2 flex ">
                America Way 2nd Floor Miami,FL 33132 United States 
